1c62f7460eaaf065a5ce5e0f1e8578278c0156f1
Burhan Tuerker <burhan.tuerker@gmail.com>
2022-03-18 16:24:54 +0300

    create docs build destination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const path = require("path")
+const fs = require("fs")
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -6,4 +9,24 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+}
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"), {
+    overwrite: true,
+  })
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
 }

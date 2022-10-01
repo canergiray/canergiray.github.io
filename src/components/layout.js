950f5e46857f77f1cd450ccbd5af2787b43d9d9a
Han Tuerker <burhan.tuerker@gmail.com>
2022-10-01 06:53:42 +0300

    update prices

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,25 +7,25 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle="Günbatımı Cafe & Restaurant" />
       <div
         style={{
           margin: `0 auto`,
